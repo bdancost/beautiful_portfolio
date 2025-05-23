a0d2007fd9f74baff3d46870f8862030ee44e869
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-05-23 20:46:50 -0300

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
