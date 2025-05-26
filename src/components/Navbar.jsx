33d1039e503051bf389b8badb719839bb8c29959
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-05-26 06:47:27 -0300

    Adicionando Component Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils";
+import { useState, useEffect } from "react";
+
+const navItems = [
+  { name: "Home", href: "#home" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
+];
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav
+      className={cn(
+        "fixed w-full z-40 transition-all duration-300",
+        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+      )}
+    >
+      <div className="container mx-auto flex items-center justify-between">
+        <a
+          className="text-xl font-bold text-primary flex items-center"
+          href="#hero"
+        >
+          <span className="relative z-10">
+            <span className="text-glow text-foreground">Daniel Fernandes</span>{" "}
+            Portfolio
+          </span>
+        </a>
+
+        {/* Desktop Nav */}
+
+        {/* Mobile Nav */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
