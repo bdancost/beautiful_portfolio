6abac04beaf8db9f4e92cd54ece748954c6c0b98
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-05-30 23:16:29 -0300

    Configurando component Aboutsection

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,7 +1,42 @@
 const AboutSection = () => {
   return (
     <section id="about" className="py-24 px-4 relative">
-      <div></div>
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary">Me</span>
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">
+              Pasionate Web Developer & Tech Creator
+            </h3>
+            <p className="text-muted-foreground">
+              With over 5 years of experience in web development, I specialize
+              in creating responsive, accessible, and performant web
+              applications using technologies.
+            </p>
+            <p className="text-muted-foreground">
+              I'm passionate about creating elegant solutions to complex
+              problems, and I'm constantly learning new technologies and
+              techniques to stay at the forefront of the ever-evolving web
+              lanscape.
+            </p>
+            <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+              <a href="#contact" className="cosmic-button">
+                Get In Touch
+              </a>
+
+              <a
+                href=""
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </section>
   );
 };
