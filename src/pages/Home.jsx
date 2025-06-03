97de1b305d45ff8a5e6edc5808d33ad2f3ea9d44
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-06-03 11:37:12 -0300

    Definindo component ConstactSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import HeroSection from "@/components/HeroSection";
 import AboutSection from "../components/AboutSection";
 import SkillsSection from "../components/SkillsSection";
 import ProjectsSection from "../components/ProjectsSection";
+import ContactSection from "../components/ContactSection";
 const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -20,6 +21,7 @@ const Home = () => {
         <AboutSection />
         <SkillsSection />
         <ProjectsSection />
+        <ContactSection />
       </main>
       {/* Footer */}
     </div>
