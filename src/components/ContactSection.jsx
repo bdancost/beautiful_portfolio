b65233ff4bcff17e1f2d06679c016ed5f2bc709d
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-06-03 12:45:04 -0300

    Refatorando o codigo e corrigindo erros

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -10,7 +10,7 @@ import {
 } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import useToast from "@/hooks/use-toast";
+import { useToast } from "@/hooks/use-toast";
 
 const ContactSection = () => {
   const { toast } = useToast();
@@ -133,13 +133,10 @@ const ContactSection = () => {
             </div>
           </div>
 
-          <div
-            className="bg-card p-8 rounded-lg shadow-xs"
-            onSubmit={handleSubmit}
-          >
+          <div className="bg-card p-8 rounded-lg shadow-xs">
             <h3 className="text-2xl font-semibold mb-6">Send a Message</h3>
 
-            <form className="space-y-6">
+            <form className="space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label
                   htmlFor="name"
